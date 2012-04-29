57d8991a3fdb17a3774674e271b5cf77d6e65a75
masdiblogs <masdiblogs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-29 17:12:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-lightbox-2/trunk@537938 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lightbox.js
+++ lightbox.js
@@ -7,19 +7,22 @@
  * Author URI:    http://onlinewebapplication.com/
  * License:       GNU General Public License, v2 (or newer)
  * License URI:  http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * Based on Lightbox 2 by Lokesh Dhakar (http://www.huddletogether.com/projects/lightbox2/)
+ */
+/*  Copyright 2011 Pankaj Jha (onlinewebapplication.com)
 
-* This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
-**/
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation using version 2 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
  
 var overlayOpacity = 0.8;	
