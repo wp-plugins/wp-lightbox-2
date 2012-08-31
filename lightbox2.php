fa4b3d982334cdb600698dfd689f0e30225c7c48
masdiblogs <masdiblogs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-31 03:19:33 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-lightbox-2/tags/2.0@592779 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lightbox2.php
@@ -0,0 +1,103 @@
+<?php
+/*
+ * Plugin Name: WP Lightbox 2
+ * Plugin URI: http://onlinewebapplication.com/2011/11/wp-lightbox-2.html
+ * Description: This plugin used to add the lightbox (overlay) effect to the current page images on your WordPress blog.
+ * Version:       2.0
+ * Author:        Pankaj Jha
+ * Author URI:    http://onlinewebapplication.com/
+ * License:       GNU General Public License, v2 (or newer)
+ * License URI:  http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * This program was modified from Ajax Contact Form plugin, version 1.0.0, 
+ * Copyright (C) 2011 Richard Gigs, released under the GNU General Public License.
+*/
+
+/* Where our theme reside: */
+$lightbox_2_theme_path = (dirname(__FILE__)."/Themes");
+update_option('lightbox_2_theme_path', $lightbox_2_theme_path);
+/* Set the default theme to Black */
+add_option('lightbox_2_theme', 'Black');
+add_option('lightbox_2_automate', 1);
+add_option('lightbox_2_resize_on_demand', 0);
+
+/* use WP_PLUGIN_URL if version of WP >= 2.6.0. If earlier, use wp_url */
+if($wp_version >= '2.6.0') {
+	$stimuli_lightbox_plugin_prefix = WP_PLUGIN_URL."/wp-lightbox-2/"; /* plugins dir can be anywhere after WP2.6 */
+} else {
+	$stimuli_lightbox_plugin_prefix = get_bloginfo('wpurl')."/wp-content/plugins/wp-lightbox-2/";
+}
+
+/* options page (required for saving prefs)*/
+$options_page = get_option('siteurl') . '/wp-admin/admin.php?page=wp-lightbox-2/options.php';
+/* Adds our admin options under "Options" */
+function lightbox_2_options_page() {
+	add_options_page('Lightbox Options', 'Lightbox 2', 10, 'wp-lightbox-2/options.php');
+}
+
+function lightbox_styles() {
+	/* What version of WP is running? */
+	global $wp_version;
+	global $stimuli_lightbox_plugin_prefix;
+    /* The next line figures out where the javascripts and images and CSS are installed,
+    relative to your wordpress server's root: */
+    $lightbox_2_theme = urldecode(get_option('lightbox_2_theme'));
+    $lightbox_style = ($stimuli_lightbox_plugin_prefix."Themes/".$lightbox_2_theme."/");
+
+    /* The xhtml header code needed for lightbox to work: */
+	$lightboxscript = "
+	<!-- begin lightbox scripts -->
+	<script type=\"text/javascript\">
+    //<![CDATA[
+    document.write('<link rel=\"stylesheet\" href=\"".$lightbox_style."lightbox.css\" type=\"text/css\" media=\"screen\" />');
+    //]]>
+    </script>
+	<!-- end lightbox scripts -->\n";
+	/* Output $lightboxscript as text for our web pages: */
+	echo($lightboxscript);
+}
+
+/* Added a code to automatically insert rel="lightbox[nameofpost]" to every image with no manual work. 
+If there are already rel="lightbox[something]" attributes, they are not clobbered. 
+Michael Tyson, you are a regular expressions god! ;) 
+http://atastypixel.com
+*/
+function autoexpand_rel_wlightbox ($content) {
+	global $post;
+	$pattern        = "/(<a(?![^>]*?rel=['\"]lightbox.*)[^>]*?href=['\"][^'\"]+?\.(?:bmp|gif|jpg|jpeg|png)['\"][^\>]*)>/i";
+	$replacement    = '$1 rel="lightbox['.$post->ID.']">';
+	$content = preg_replace($pattern, $replacement, $content);
+	return $content;
+}
+
+if (get_option('lightbox_2_automate') == 1){
+	add_filter('the_content', 'autoexpand_rel_wlightbox', 99);
+	add_filter('the_excerpt', 'autoexpand_rel_wlightbox', 99);
+}
+
+/* To resize images, or not to resize; that is the question */
+$resize_images_or_not = get_option('lightbox_2_resize_on_demand');
+if ($resize_images_or_not == 1) {
+	$stimuli_lightbox_js = "lightbox-resize.js"; 
+} else {
+	$stimuli_lightbox_js = "lightbox.js"; 
+}
+
+if (!is_admin()) { // if we are *not* viewing an admin page, like writing a post or making a page:
+	wp_enqueue_script('lightbox', ($stimuli_lightbox_plugin_prefix.$stimuli_lightbox_js), array('scriptaculous-effects'), '1.8');
+}
+
+/* we want to add the above xhtml to the header of our pages: */
+add_action('wp_head', 'lightbox_styles');
+add_action('admin_menu', 'lightbox_2_options_page');
+?>
---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== WP Lightbox 2 ===
+Contributors: Pankaj Jha
+Plugin Site: http://onlinewebapplication.com/
+Tags: lightbox, WP Lightbox, AJAX, image, photo, picture, JQuery WP Image Plugin
+Requires at least: 1.5
+Tested up to: 3.2.1
+Stable tag: 3.0.0
+
+This plugin used to add the lightbox (overlay) effect to the current page images on your WordPress blog.
+== Description ==
+<br />
+[Author Site](http://onlinewebapplication.com)|
+[Plugin Home Page](http://onlinewebapplication.com/2011/11/wp-lightbox-2.html)
+<br />
+<br />
+This plugin used to add the lightbox (overlay) effect to the current page images on your WordPress blog.
+Used to overlay images on the current page. Extension of Lightbox 2 which features 'auto-lightboxing' of image links, courtesy of Michael Tyson ( http://atastypixel.com ).
+
+Just install and sit back. This plugin enable image overlay lighbox effect for all the post images in your wordpress plugin. No configuration required.
+
+== Installation ==
+
+To do a new installation of the plugin, please follow these steps
+
+1. Download the zipped plugin file to your local machine.
+2. Unzip the file.
+3. Upload the `WP-lightbox-2` folder to the `/wp-content/plugins/` directory.
+4. Activate the plugin through the 'Plugins' menu in WordPress.
+5. Optionally, go to the Options page and select a new Lightbox colour scheme.
+
+
+== Frequently Asked Questions ==
+
+You can Check Screen Shots on my website :<br />
+[WP Lightbox 2](http://onlinewebapplication.com/2011/11/wp-lightbox-2.html)<br />
+
+== Screenshots ==
+You can Check Screen Shots on my website :<br />
+[WP Lightbox 2](http://onlinewebapplication.com/2011/11/wp-lightbox-2.html)<br />
